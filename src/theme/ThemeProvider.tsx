33f9707cf409ce2eb16dbae37c1da1454a0403c8
mahdi <you@example.com>
2022-07-20 18:07:14 +0430

    theme provider unstable

--- src/theme/ThemeProvider.tsx
+++ src/theme/ThemeProvider.tsx
@@ -1,8 +1,64 @@
-export const theme = {
-    light:{
-        
-    },
-    dark:{
+const dark = {
+  50: "#e5e6e8",
+  100: "#bec1c5",
+  200: "#92979f",
+  300: "#666d78",
+  400: "#464e5b",
+  500: "#252f3e",
+  600: "#212a38",
+  700: "#1b2330",
+  800: "#161d28",
+  900: "#0d121b",
+  A100: "#5d8eff",
+  A200: "#2a6aff",
+  A400: "#004af6",
+  A700: "#0042dd",
+};
 
-    }
-}
+const lightText = {
+  primary: "rgb(17,24,39)",
+  secondary: "rgb(107,114,128)",
+  disabled: "rgb(149,156,169)",
+};
+const darkText = {
+  primary: "rgb(255,255,255)",
+  secondary: "rgb(229,231,235)",
+  disabled: "rgb(156,163,175)",
+};
+
+export const themeConfig = {
+  default: {
+    palette: {
+      type: "light",
+      text: lightText,
+      primary: dark,
+      secondary: {
+        light: "#e4fafd",
+        main: "#22d3ee",
+        dark: "#0cb7e2",
+      },
+      background: {
+        paper: "#FFFFFF",
+        default: "#f6f7f9",
+      },
+      error: "red",
+    },
+  },
+  darkDefault: {
+    palette: {
+      type: "dark",
+      text: darkText,
+      primary: dark,
+      secondary: {
+        light: "#e4fafd",
+        main: "#22d3ee",
+        dark: "#0cb7e2",
+      },
+      background: {
+        paper: "#1E2125",
+        default: "#121212",
+      },
+      error: "red",
+    },
+  },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: {
+          100: "rgb(241,245,249)",
+          900: "rgb(255,255,255)",
+        },
+        dark: {
+          100: "rgb(51,66,84)",
+          200: "rgb(17,24,38)",
+          300: "rgb(30,41,58)",
+        },
+      },
+    },
   },
   plugins: [],
 };
